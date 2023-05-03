79e2e12ec44105670701af19db911ce3bc4defc3
Mortafix <moris.doratiotto@gmail.com>
2023-05-03 11:27:33 +0200

    Fix: cache delay decreased

--- utils/api.py
+++ utils/api.py
@@ -15,6 +15,8 @@ BASE_ATTRIBUTES = [
     "battle-rank",
     "speciality",
 ]
+LITTLE_DELAY = 60 * 2  # 2 minutes
+BIG_DELAY = 60 * 60 * 1  # 1 hour
 
 # ---- Main
 
@@ -28,14 +30,14 @@ def api_call(endpoint, method=get, payload=None, **params):
 # ---- Base
 
 
-@st.cache(ttl=60 * 60 * 60 * 24, show_spinner=False)
+@st.cache(ttl=BIG_DELAY, show_spinner=False)
 def get_kryptomon_images(kmon_id):
     images = api_call(f"/kryptomons/images/{kmon_id}").get("data")
     image = images.get("junior") or images.get("baby") or images.get("egg")
     return image.get("png-image")
 
 
-@st.cache(ttl=60 * 60 * 5, show_spinner=False)
+@st.cache(ttl=LITTLE_DELAY, show_spinner=False)
 def get_kryptomon_game_stats(kmon_id):
     return api_call(f"/kryptomons/game/stats/{kmon_id}").get("data")
 
@@ -43,7 +45,7 @@ def get_kryptomon_game_stats(kmon_id):
 # ---- Funcs
 
 
-@st.cache(ttl=60 * 60 * 5, show_spinner=False)
+@st.cache(ttl=LITTLE_DELAY, show_spinner=False)
 def get_dashboard_wallet(wallet):
     return api_call(f"/user/{wallet}")
 
@@ -57,7 +59,7 @@ def get_kryptomon_ranks(kmon_ids):
     return api_call("/kryptomons/v2", method=post, payload=body)
 
 
-@st.cache(ttl=60 * 60 * 10, show_spinner=False)
+@st.cache(ttl=LITTLE_DELAY, show_spinner=False)
 def get_top_kryptomons(primary_elem=None, secondary_elem=None):
     filters = dict()
     if primary_elem:
